7c05ba4fd8c8fac2fc7a2c8e176e23eda4c09d6a
eldenaspataceanu <eldenasp@yahoo.com>
2022-09-19 20:55:03 +0300

    Basic

--- homework - data_structure/main.py
+++ homework - data_structure/main.py
@@ -1,10 +1,8 @@
 my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
-
-print(my_list[::])   # Afiseaza lista ordonata ascendent
-print(my_list[::-1])    # Afiseaza lista ordonata descendent
-
 my_list_sort = sorted(my_list)
 
+print(my_list_sort[::])   # Afiseaza lista ordonata ascendent
+print(my_list_sort[::-1])    # Afiseaza lista ordonata descendent
 print(my_list_sort[1::2])   # Afiseaza lista cu numere pare
 print(my_list_sort[::2])    # Afiseaza lista cu numerele impare
 print(my_list_sort[2:10:3]) # Afiseaza lista cu numere ce sunt multipli de 3
